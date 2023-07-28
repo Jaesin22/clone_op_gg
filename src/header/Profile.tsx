e1225bbc55b23350ca1fe5ac5d1749554f0c7216
HyeonJae-Kim <hyeonjae.kim@telusinternational.com>
2023-07-28 18:03:11 +0900

    Profile 만드는 중

--- src/header/Profile.tsx
+++ src/header/Profile.tsx
@@ -39,19 +39,46 @@ const Profile = () => {
                 </li>
               </ul>
             </div>
-            <div className="name mt-2">
-              <h1 className="summoner-name mr-2 text-2xl font-bold">
-                슬기로운현재
-              </h1>
-              <button className="w-7 h-7 rounded border-solid border border-[#DBE0E4] ">
+            <div className="flex name mt-2">
+              <h1 className="summoner-name text-2xl font-bold">슬기로운현재</h1>
+              <button className="w-7 h-7 rounded border-solid border ml-2 mt-0.5 border-[#DBE0E4]">
                 <img
                   src="https://s-lol-web.op.gg/images/icon/icon-bookmark.svg?v=1690447902108"
                   alt="off"
                 />
               </button>
             </div>
-            <div className="advertise"></div>
-            <div className="reload_button"></div>
+            <div className="advertise mt-2 items-center">
+              <a
+                href="!#"
+                className="flex border border-[#DBE0E4] bg-white p-1.5 rounded"
+              >
+                <img
+                  src="https://s-lol-web.op.gg/assets/images/icons/riot-logomark-circle-offwhite@2x.png?image=q_auto,f_webp,w_32&v=1690447901880"
+                  alt="logo"
+                  className="w-4 h-4"
+                />
+                <span className="text-xs ml-2">
+                  라이엇 계정 연동하고 나만의 프로필을 설정해보세요.
+                </span>
+                <img
+                  src="https://s-lol-web.op.gg/images/icon/icon-arrow-right.svg?v=1690447901880"
+                  alt=">"
+                  className="ml-2.5 w-5 h-5"
+                />
+              </a>
+            </div>
+            <div className="reload_button mt-2">
+              <button
+                type="button"
+                className="rounded text-white bg-[#5383e9] py-2.5 px-3 text-sm"
+              >
+                전적 갱신
+              </button>
+            </div>
+            <div className="last-update mt-1.5 text-xs text-gray-400">
+              <div>최근 업데이트 : 3일 전</div>
+            </div>
           </div>
         </div>
       </div>
